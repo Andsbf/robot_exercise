4134012f837ec2fdd26e91950306e201a85da26a
Andsbf <andsbf@gmail.com>
2015-03-12 22:02:01 +0000

    specify error classes in the enhanciments test

--- spec/11_robot_enhanciments.rb
+++ spec/11_robot_enhanciments.rb
@@ -13,7 +13,7 @@ describe Robot do
     it "return raised flag if robot already deade" do
       @robot.wound(999)
       
-      expect{@robot.heal!}.to raise_error 
+      expect{@robot.heal!}.to raise_error(RobotAlreadyDeadError)
     end
   end
 
@@ -21,7 +21,7 @@ describe Robot do
     
     it "raise error when enemy is not a robot" do
       book = double("Book")
-      expect{@robot.attack!(book)}.to raise_error
+      expect{@robot.attack!(book)}.to raise_error(EnemyHasToBeRobotError)
     end
 
     it "Do not raise error when enemy is  a robot" do
@@ -37,6 +37,13 @@ describe Robot do
 
 
   end
+class RobotAlreadyDeadError < StandardError
+end
+
+class EnemyHasToBeRobotError < StandardError
+end
+
+
 
 
 
